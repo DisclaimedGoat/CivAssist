bf05b95e986aceefad02dda55b4dc04460012584
DisclaimedGoat <kyleturleywas@gmail.com>
2022-05-05 20:43:28 -0600

    Another another another listener test

--- civassist/src/main/java/com/disclaimedgoat/Utilities/Network/CivListener.java
+++ civassist/src/main/java/com/disclaimedgoat/Utilities/Network/CivListener.java
@@ -1,6 +1,9 @@
 package com.disclaimedgoat.Utilities.Network;
 
 import com.disclaimedgoat.Utilities.DataManagement.Logger;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
@@ -16,12 +19,18 @@ public class CivListener extends AbstractHandler {
 
         request.setHandled(true);
         httpServletResponse.setStatus(200);
-        Logger.globalLogF("server", "Receiving request from '%s'", request.getRemoteAddr());
+        Logger.globalLogF("server", "Receiving request from '%s'", request.getRemoteHost());
 
         try {
             BufferedReader out = httpServletRequest.getReader();
+            JsonObject jsonObject = JsonParser.parseReader(out).getAsJsonObject();
+            System.out.println(jsonObject.getAsString());
 
-            out.lines().forEach(System.out::println);
+            String gameName = jsonObject.get("value1").getAsString();
+            String playerName = jsonObject.get("value2").getAsString();
+            String turnNumber = jsonObject.get("value3").getAsString();
+
+            System.out.printf("It's %s's turn for game %s. Turn number: %s%n", playerName, gameName, turnNumber);
 
         } catch (IOException e) {
             e.printStackTrace();
